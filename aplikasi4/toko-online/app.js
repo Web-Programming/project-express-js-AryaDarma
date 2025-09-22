98c1296330449ef53a04f3628ec98813714a0f14
shela-2226250087 <shela_2226250087@mhs.mdp.ac.id>
2025-09-22 17:05:31 +0700

    toko-online

--- aplikasi4/toko-online/app.js
+++ aplikasi4/toko-online/app.js
@@ -6,13 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var engine = require('ejs-blocks');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.set('view engine','ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- aplikasi4/toko-online/routes/index.js
+++ aplikasi4/toko-online/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var product= require('../data/product.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Toko online Sederhana', products: product });
 });
 
 module.exports = router;
